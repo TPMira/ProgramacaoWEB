53248d95222fbcfa33d98dc5695544eeed58bcc0
FurtadoPL <62638080+FurtadoPL@users.noreply.github.com>
2020-03-27 21:42:53 -0300

    Add files via upload
    Sem o uso da API Calendar.

--- src/Data.java
+++ src/Data.java
@@ -1,70 +1,76 @@
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-@WebServlet("/Data")
-public class Data extends HttpServlet {
-	private static final long serialVersionUID =5120121212L;
-       
-    
-    public Data() {
-        super();
-       
-    }
-
+public class Data{
 	
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-    }
-
+	private byte dia;
+	private byte mes;
+	private short ano;
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		//doGet(request, response);
-		
-		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-	      Date dataNascimento = null;
-		try {
-			dataNascimento = formato.parse(request.getParameter("num"));
-		} catch (ParseException e) {
-			
-			e.printStackTrace();
-		} 
-	      int idade = calculaIdade(dataNascimento);
-	      //variavel que recebe idade:
-	      
-	      PrintWriter out = response.getWriter();
-	      out.println("Sua idade é de "+idade+" anos");
-	   }
-
-	public static int calculaIdade(java.util.Date dataN) {
-
-	    Calendar dataNasc = Calendar.getInstance();  
-	    dataNasc.setTime(dataN); 
-	    Calendar dataAtual = Calendar.getInstance();  
-
-	    int idade = dataAtual.get(Calendar.YEAR) - dataNasc.get(Calendar.YEAR); 
-
-	    if (dataAtual.get(Calendar.MONTH) < dataNasc.get(Calendar.MONTH)) {
-	      idade--;  
-	    } 
-	    else
-	    { 
-	        if (dataAtual.get(Calendar.MONTH) == dataNasc.get(Calendar.MONTH) && dataAtual.get(Calendar.DAY_OF_MONTH) < dataNasc.get(Calendar.DAY_OF_MONTH)) {
-	            idade--; 
-	        }
-	    }
-
-	    return idade;
+	public Data(int dia, int mes, int ano ) {
+		setData31(dia, mes, ano);
+		setData30(dia, mes, ano);
+		setDataFevereiro(dia, mes, ano);
 	}
+	
+	
+	public void setData31(int dia, int mes, int ano) {
+		if (ano >= 1500 && ano <= 2020) {
+			this.ano= (short)ano;
+			}else {
+			throw new RuntimeException("O ano "+ano+" é  invalido");
+			}
+			if(mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12) {
+				this.mes= (byte)mes;
+				}else if (mes < 1 || mes > 12) {
+				throw new RuntimeException("O mes "+mes+" é invalido");
+				}if(dia>=1 && dia<=31) {
+					this.dia = (byte)dia;
+					}else {
+						throw new RuntimeException("O valor "+dia+" é um dia invalido");
+						}
+				
+			}
+		
+	
+	
+	
+	public void setData30(int dia, int mes, int ano) {
+		if (ano >= 1500 && ano <= 2020) {
+			this.ano= (short)ano;
+			}else {
+			throw new RuntimeException("O ano "+ano+" é  invalido");
+			}
+			if(mes==4 || mes==6 || mes==9 || mes==11) {
+				this.mes= (byte)mes;
+			}else if (mes < 1 || mes > 12) {
+				throw new RuntimeException("O mes "+mes+" é invalido");
+			}if(dia>=1 && dia<=30) {
+					this.dia = (byte)dia;
+					}else {
+						throw new RuntimeException("O valor "+dia+" é um dia invalido");
+						}
+				
+			}
+		
+	public void setDataFevereiro(int dia, int mes, int ano) {
+	 if (ano >= 1500 && ano <= 2020 ){
+			this.ano= (short)ano;
+			if(mes==2) {
+				this.mes= (byte)mes;
+				}else if (mes < 1 || mes > 12) { 
+				throw new RuntimeException("O mes "+mes+" é invalido");
+				}if(dia>=1 && dia<=29 ) {
+					this.dia = (byte)dia;
+					}else {
+						throw new RuntimeException("O dia "+dia+" é invalido");
+					}
+			
+				
+			}
+		}
+	@Override
+	public String toString() {
+		return this.dia + "/" + this.mes +"/"+ this.ano;
+	}
+
 }
+
+
---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+	public static void main(String[] args) {
+		byte dia = 30;
+		byte mes = 2;
+		short ano = 2020;
+		
+		
+		Data d  = new Data( dia,  mes,  ano);
+		System.out.println(d);
+	}
+
+}
